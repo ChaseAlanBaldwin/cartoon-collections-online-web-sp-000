879ee7107119ad58a3305d2216c48188b6ec6fbb
Chase-Alan Baldwin <alan.chasebaldwin@gmail.com>
2020-11-09 04:44:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,13 @@ def long_planeteer_calls(calls)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  cheese_types.each do |i|
+    if cheese.include(i) == true
+      true
+    else
+      nil
+    end
 end
