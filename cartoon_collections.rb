f99962b76a89dba47bda87da358ffee13a5a8a83
Chase-Alan Baldwin <alan.chasebaldwin@gmail.com>
2020-11-09 04:32:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@ def roll_call_dwarves(names)
   names.each_with_index {|i, a| puts "#{a + 1}. #{i}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+
 end
 
 def long_planeteer_calls# code an argument here
